f9beb15881d98a632580633549044aad3a03236c
TBI89 <tomerben89@gmail.com>
2023-04-27 12:51:59 +0300

    New assignment

---
+++ JavaScript- Flow charts/JavaScript-Print power/main.js
@@ -0,0 +1,12 @@
+// Print power
+
+let num = +prompt("Enter a number: ");
+let result = 0;
+
+while (num >= 0) {
+   result = num ** 3;
+   document.write(`${num ** 3}<br>`);
+   result = result + 1;
+    num = +prompt("Enter a number :");
+}
+
