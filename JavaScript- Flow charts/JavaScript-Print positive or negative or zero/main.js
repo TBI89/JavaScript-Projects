51d92d6882a75fb02db2fa965c10bf02bca5d064
TBI89 <tomerben89@gmail.com>
2023-04-27 15:28:53 +0300

    New assignments

---
+++ JavaScript- Flow charts/JavaScript-Print positive or negative or zero/main.js
@@ -0,0 +1,30 @@
+// Print positive, negative or zero
+
+let positiveCount = "";
+let negativeCount = "";
+let zeroCount = "";
+let num;
+
+while(true) {
+    num = +prompt("Enter a number :");
+
+    if(num % 7 === 0) {
+        break;
+    }
+
+    if (num > 0) {
+        positiveCount += num + ",";
+    }
+
+    else if (num < 0) {
+        negativeCount += num + ",";
+    }
+
+    else {
+        zeroCount += num + ",";
+    }
+}
+
+document.write(`Positive numbers entered: ${positiveCount}<br>`);
+document.write(`Negative numbers entered: ${negativeCount}<br>`);
+document.write(`Zeros entered: ${zeroCount}<br>`);
